290ed42a03e1d06c5b852decd3d778cbecb9d79c
emirhan-coban <emirhancob4n@gmail.com>
2025-11-24 21:31:39 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Yazdırma Komutları
+            //Console.WriteLine("Hello, World!");
+            //Console.WriteLine();
+            //Console.WriteLine("C# Programlama Dili'ne Hoşgeldiniz...");
+            //Console.WriteLine();
+            //Console.WriteLine("Bugün hava çok güzel.");
+            #endregion
+
+            #region Değişkenler ve Veri Tipleri
+            //string
+            //string name = "Emirhan";
+            //Console.WriteLine("İsim: " + name);
+
+            //// int
+            //int age = 25;
+            //Console.WriteLine("Yaş: " + age);
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
+
+
+
+
+// Yazdırma Komutları
